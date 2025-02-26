1122eec900262e0f487424a878b3c6ec95c01ab8
Park Hyunwoo <hyunwoo.park@izex.co.kr>
2025-02-26 11:05:31 +0900

    Hello World

--- src/main/java/com/example/demo/DefaultApplication.java
+++ src/main/java/com/example/demo/DefaultApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DefaultApplication {
-
+	public DefaultApplication() {
+		super();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DefaultApplication.class, args);
 	}
